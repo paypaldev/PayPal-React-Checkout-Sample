838e5801393668aafc6f5d495369738b6f4a0ab9
Pato Vargas <pevargas2730@gmail.com>
2022-10-16 17:04:59 -0400

    usePayPalScriptReducer loader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Checkout from './Checkout';
 const initialOptions = {
-  "client-id": "YOUR-CLIENT-ID-HERE",
+  "client-id": "ASBRTbGDIp10_WxZj0lR6P7CymT4JEgkdP_B-x5k6jRxZ0B_eW8_Cl3c8VPqVQlBb4GIgYAS9gl9-bXP",
   currency: "USD",
   intent: "capture",
 };
 
 function App() {
-  
   return (
     <PayPalScriptProvider options={initialOptions}>
-        <PayPalButtons style={{ layout: "vertical" }} />
+        <Checkout/>
     </PayPalScriptProvider>
   );
 }
---
+++ src/Checkout.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import './Checkout.css';  
+
+const Checkout = () => {
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+    const [currency, setCurrency] = useState(options.currency);
+
+    const onCurrencyChange = ({ target: { value } }) => {
+        setCurrency(value);
+        dispatch({
+            type: "resetOptions",
+            value: {
+                ...options,
+                currency: value,
+            },
+        });
+    }
+
+    return (
+        <div className="checkout">
+            {isPending ? <p>LOADING...</p> : (
+                <>
+                    <select value={currency} onChange={onCurrencyChange}>
+                            <option value="USD">💵 USD</option>
+                            <option value="EUR">💶 Euro</option>
+                    </select>
+                    <PayPalButtons style={{ layout: "vertical" }}/>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default Checkout;
