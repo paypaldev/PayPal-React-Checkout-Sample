921c68b3eca67ecefab6ff65581591d796b5074f
Pato Vargas <pevargas2730@gmail.com>
2022-10-16 17:40:23 -0400

    onCreateOrder and onApprove transaction

--- src/Checkout.js
+++ src/Checkout.js
@@ -25,7 +25,26 @@ const Checkout = () => {
                             <option value="USD">💵 USD</option>
                             <option value="EUR">💶 Euro</option>
                     </select>
-                    <PayPalButtons style={{ layout: "vertical" }}/>
+                    <PayPalButtons 
+                        style={{ layout: "vertical" }}
+                        createOrder={(data, actions) => {
+                            return actions.order.create({
+                                purchase_units: [
+                                    {
+                                        amount: {
+                                            value: "8.99",
+                                        },
+                                    },
+                                ],
+                            });
+                        }}
+                        onApprove={(data, actions) => {
+                            return actions.order.capture().then((details) => {
+                                const name = details.payer.name.given_name;
+                                alert(`Transaction completed by ${name}`);
+                            });
+                        }}
+                    />
                 </>
             )}
         </div>
