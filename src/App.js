b7503ae80202661d706df07c320156a3a878adcc
Patricio Vargas <pevargas2730@gmail.com>
2022-11-04 17:57:04 -0400

    client id

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Checkout from './Checkout';
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const initialOptions = {
-  "client-id": "ASBRTbGDIp10_WxZj0lR6P7CymT4JEgkdP_B-x5k6jRxZ0B_eW8_Cl3c8VPqVQlBb4GIgYAS9gl9-bXP",
+  "client-id": "YOUR-PAYPAL-CLIENT-ID",
   currency: "USD",
   intent: "capture",
 };
